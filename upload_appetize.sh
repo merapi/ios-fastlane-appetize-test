d2394afbb78c0d3897ca32c5da827da16b4ba5de
Marcin Pietruszka <marcin@webskill.pl>
2016-09-22 15:48:51 +0100

    fix

--- upload_appetize.sh
+++ upload_appetize.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 zip app ios/build/Build/Products/Debug-iphonesimulator/IosFastlaneAppetizeTest.app -r
 curl https://tok_99770edwpdk12h8u78nemra6xc@api.appetize.io/v1/apps -F "file=@app.zip" -F "platform=ios"
