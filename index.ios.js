76c1d1c6af3c6986b8ef9261c34456210b4b4923
Marcin Pietruszka <marcin@webskill.pl>
2016-09-23 23:42:59 +0100

    red bg, i will generate aptz because of label at this moment

--- index.ios.js
+++ index.ios.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'pink',
+    backgroundColor: 'red',
   },
   welcome: {
     fontSize: 20,
