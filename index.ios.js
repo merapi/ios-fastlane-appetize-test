ebbc09bd4aa55ac7a6b3440eeaae613c568e0e26
Marcin Pietruszka <marcin@webskill.pl>
2016-09-23 23:15:22 +0100

    change background to blue +appetize or +a

--- index.ios.js
+++ index.ios.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
+    backgroundColor: 'blue',
   },
   welcome: {
     fontSize: 20,
